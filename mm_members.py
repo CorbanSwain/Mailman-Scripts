2390e582f29e7fc803d2277d3af73cc70890091c
Corban Swain <CorbanSwain@gmail.com>
2018-09-24 09:41:11 -0400

    subscribe and list functions

--- mm_members.py
+++ mm_members.py
@@ -18,6 +18,23 @@ from utilities import *
 from string import ascii_lowercase
 
 
+def get_password(list_name):
+    list_pass = None
+    try:
+        with open('.mailman', 'r') as mm_fle:
+            for line in mm_fle.readlines():
+                if line.strip().startswith('#'):
+                    continue
+                if line.strip().startswith(list_name):
+                    list_pass = line.split(':')[1].strip()
+    except FileNotFoundError:
+        pass
+
+    if not list_pass:
+        list_pass = input('What is the "%s" list password?\n\t> ' % list_name)
+
+    return list_pass
+
 def get_admin_url(list_name):
     mm_root = 'http://mailman.mit.edu/mailman/'
     return mm_root + 'admin/' + list_name
@@ -31,7 +48,7 @@ def get_page_members(url):
 
 
 def get_all_members(list_name):
-    list_pass = input('What is the "%s" list password?' % list_name)
+    list_pass = get_password(list_name)
     url_fmt = url_change_query(get_admin_url(list_name) + '/members',
                                adminpw=list_pass,
                                letter='{:s}')
@@ -51,10 +68,37 @@ def refine_email_list(emails):
     return new_emails
 
 
+def subscribe_members(list_name, emails):
+    list_pass = get_password(list_name)
+    url_fmt = url_change_query(get_admin_url(list_name) + '/members/add',
+                               adminpw=list_pass,
+                               subscribees='{:s}')
+
+    l1 = len(emails)
+    emails = refine_email_list(emails)
+    l2 = len(emails)
+    if l2 < l1:
+        print('%d duplicate emails removed.' % (l1 - l2))
+    current_emails = refine_email_list(get_all_members(list_name))
+    emails = {email for email in emails if email not in current_emails}
+    l3 = len(emails)
+    if l3 < l2:
+        print('%d emails already in the database removed.' % (l2 - l3))
+
+    print('Subscribing %d emails to the "%s" mailman list.' % (l3, list_name))
+    [requests.get(url_fmt.format(email)) for email in emails]
+
+    updated_emails = refine_email_list(get_all_members(list_name))
+
+    [print('%s successfully added.' % email) if email in updated_emails
+     else print('%s -- FAILED TO ADD' % email) for email in emails]
+
+
 if __name__ == "__main__":
     list_name = 'bgsa'
     print('\n')
     print('Fetching Members for list: {:s}'.format(list_name))
     all_emails = refine_email_list(get_all_members(list_name))
-    pp.pprint('corbanswain@gmail.com' in all_emails)
     pp.pprint('Number of total emails: %d' % len(all_emails))
+    subscribe_members('bgsa', ['corbanswain@gmail.com',
+                               'corbanswainphoto@gmail.com'])
